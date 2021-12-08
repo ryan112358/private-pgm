8d57504ae076ae1cb54f7a1c1f2a0815a696ae77
LukeHartmanTmlt <56605420+LukeHartmanTmlt@users.noreply.github.com>
2021-12-08 13:38:55 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='private-pgm',
     version='0.0.1',
     description='Graphical-model based estimation and inference for differential privacy.',
-    url='git@github.com:LukeHartmanTmlt/private-pgm.git',
+    url='git@github.com:ryan112358/private-pgm.git',
     author='Ryan McKenna',
     author_email='rmckenna21@gmail.com',
     license='Apache License 2.0',
