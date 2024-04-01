b0bcc000d9dc068348807cebd964407deb1ee79b
ceciliaferrando <cferrando@umass.edu>
2024-04-01 15:39:55 -0400

    Adding changes to 1) output model and synth data, 2) support weighted marginals and custom initial cliques

--- mechanisms/aim.py
+++ mechanisms/aim.py
@@ -1,7 +1,7 @@
 import numpy as np
 import itertools
 from mbi import Dataset, GraphicalModel, FactoredInference, Domain
-from mechanisms.mechanism import Mechanism
+from .mechanism import Mechanism
 from collections import defaultdict
 from hdmm.matrix import Identity
 from scipy.optimize import bisect
@@ -24,11 +24,14 @@ def hypothetical_model_size(domain, cliques):
     model = GraphicalModel(domain, cliques)
     return model.size * 8 / 2**20
 
-
 def compile_workload(workload):
+    weights = {cl: wt for (cl, wt) in workload}
+    workload_cliques = weights.keys()
+
     def score(cl):
-        return sum(len(set(cl)&set(ax)) for ax in workload)
-    return { cl : score(cl) for cl in downward_closure(workload) }
+        return sum(weights[workload_cl] * len(set(cl) & set(workload_cl)) for workload_cl in workload_cliques)
+
+    return {cl: score(cl) for cl in downward_closure(workload_cliques)}
 
 def filter_candidates(candidates, model, size_limit):
     ans = { }
@@ -41,9 +44,10 @@ def filter_candidates(candidates, model, size_limit):
     return ans
 
 class AIM(Mechanism):
-    def __init__(self,epsilon,delta,prng=None,rounds=None,max_model_size=80,structural_zeros={}):
+    def __init__(self,epsilon,delta,max_iters,prng=None,rounds=None,max_model_size=80,structural_zeros={}):
         super(AIM, self).__init__(epsilon, delta, prng)
         self.rounds = rounds
+        self.max_iters = max_iters
         self.max_model_size = max_model_size
         self.structural_zeros = structural_zeros
 
@@ -61,11 +65,13 @@ class AIM(Mechanism):
         max_sensitivity = max(sensitivity.values()) # if all weights are 0, could be a problem
         return self.exponential_mechanism(errors, eps, max_sensitivity)
 
-    def run(self, data, W):
+    def run(self, data, workload, num_synth_rows, initial_cliques):
         rounds = self.rounds or 16*len(data.domain)
-        workload = [cl for cl, _ in W]
         candidates = compile_workload(workload)
-        answers = { cl : data.project(cl).datavector() for cl in candidates }
+        answers = {cl : data.project(cl).datavector() for cl in candidates}
+
+        if not initial_cliques:
+            initial_cliques = [cl for cl in candidates if len(cl) == 1]  # use one-way marginals
 
         oneway = [cl for cl in candidates if len(cl) == 1]
 
@@ -75,14 +81,14 @@ class AIM(Mechanism):
         measurements = []
         print('Initial Sigma', sigma)
         rho_used = len(oneway)*0.5/sigma**2
-        for cl in oneway:
+        for cl in initial_cliques:
             x = data.project(cl).datavector()
             y = x + self.gaussian_noise(sigma,x.size)
             I = Identity(y.size) 
             measurements.append((I, y, sigma, cl))
 
         zeros = self.structural_zeros
-        engine = FactoredInference(data.domain,iters=1000,warm_start=True,structural_zeros=zeros)
+        engine = FactoredInference(data.domain,iters=self.max_iters,warm_start=True,structural_zeros=zeros)
         model = engine.estimate(measurements)
 
         t = 0
@@ -111,18 +117,18 @@ class AIM(Mechanism):
 
             model = engine.estimate(measurements)
             w = model.project(cl).datavector()
-            print('Selected',cl,'Size',n,'Budget Used',rho_used/self.rho)
+            #print('Selected',cl,'Size',n,'Budget Used',rho_used/self.rho)
             if np.linalg.norm(w-z, 1) <= sigma*np.sqrt(2/np.pi)*n:
                 print('(!!!!!!!!!!!!!!!!!!!!!!) Reducing sigma', sigma/2)
                 sigma /= 2
                 epsilon *= 2
 
         print('Generating Data...')
-        engine.iters = 2500
+        engine.iters = self.max_iters
         model = engine.estimate(measurements)
-        synth = model.synthetic_data()
+        synth = model.synthetic_data(rows=num_synth_rows)
 
-        return synth
+        return model, synth
 
 def default_params():
     """
@@ -170,7 +176,7 @@ if __name__ == "__main__":
 
     workload = [(cl, 1.0) for cl in workload]
     mech = AIM(args.epsilon, args.delta, max_model_size=args.max_model_size)
-    synth = mech.run(data, workload)
+    model, synth = mech.run(data, workload)
 
     if args.save is not None:
         synth.df.to_csv(args.save, index=False)
@@ -182,3 +188,4 @@ if __name__ == "__main__":
         e = 0.5*wgt*np.linalg.norm(X/X.sum() - Y/Y.sum(), 1)
         errors.append(e)
     print('Average Error: ', np.mean(errors))
+
