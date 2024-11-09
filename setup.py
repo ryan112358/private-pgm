e05aaa7c2da8a7d8ab8425eb2c5ad50be5b52f6c
Ryan McKenna <rmckenna21@gmail.com>
2024-11-09 07:14:58 -0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
     name='private-pgm',
-    version='0.0.1',
+    version='0.0.2',
     description='Graphical-model based estimation and inference for differential privacy.',
     url='git@github.com:ryan112358/private-pgm.git',
     author='Ryan McKenna',
     author_email='rmckenna21@gmail.com',
     license='Apache License 2.0',
-    packages=['mbi'],
+    packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=requirements,
 )
