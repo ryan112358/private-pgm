f52c36ba7935b458eab2dd148264bd3d10d10072
Ryan McKenna <rmckenna21@gmail.com>
2024-11-13 20:26:34 -0800

    add additional estimation algorithms and tests

--- src/mbi/estimation.py
+++ src/mbi/estimation.py
@@ -1,3 +1,18 @@
+"""Algorithms for estimating graphical models from marginal-based loss functions.
+
+This module provides a flexible set of optimization algorithms, each sharing the
+the same API.  The supported algorithms are:
+    1. Mirror Descent [our recommended algorithm]
+    2. L-BFGS (using back-belief propagation)
+    3. Regularized Dual Averaging
+    4. Interior Gradient
+
+Each algorithm can be given an initial set of potentials, or can automatically
+intialize the potentials to zero for you.  Any CliqueVector of potentials that
+support the cliques of the marginal-based loss function can be used here.
+"""
+
+
 import numpy as np
 from mbi import Domain, CliqueVector, Factor, LinearMeasurement
 from mbi import marginal_oracles, marginal_loss, synthetic_data
@@ -5,6 +20,7 @@ from typing import Callable
 import jax
 import chex
 import attr
+import optax
 
 _DEFAULT_CALLBACK = lambda t, loss: print(loss) if t % 50 == 0 else None
 
@@ -57,16 +73,7 @@ def minimum_variance_unbiased_total(measurements: list[LinearMeasurement]) -> fl
         return max(1, estimate)
 
 
-def mirror_descent(
-    domain: Domain,
-    loss_fn: marginal_loss.MarginalLossFn | list[LinearMeasurement],
-    known_total: float | None = None,
-    potentials: CliqueVector | None = None,
-    marginal_oracle=marginal_oracles.message_passing,
-    iters: int = 1000,
-    stepsize: float | None = None,
-    callback_fn: Callable[[int, chex.Numeric], None] = _DEFAULT_CALLBACK,
-):
+def _initialize(domain, loss_fn, known_total, potentials):
     if isinstance(loss_fn, list):
         if known_total is None:
             known_total = minimum_variance_unbiased_total(loss_fn)
@@ -77,18 +84,59 @@ def mirror_descent(
     if potentials is None:
         potentials = CliqueVector.zeros(domain, loss_fn.cliques)
 
-    if stepsize is None:
-        stepsize = 2.0  # 2.0 / known_total  # what should this be?
+    return loss_fn, known_total, potentials
 
-    loss_and_grad = jax.value_and_grad(loss_fn)
+
+def mirror_descent(
+    domain: Domain,
+    loss_fn: marginal_loss.MarginalLossFn | list[LinearMeasurement],
+    known_total: float | None = None,
+    potentials: CliqueVector | None = None,
+    marginal_oracle=marginal_oracles.message_passing_new,
+    iters: int = 1000,
+    stepsize: float | None = None,
+    callback_fn: Callable[[int, chex.Numeric], None] = _DEFAULT_CALLBACK,
+):
+    """Optimization using the Mirror Descent algorithm.
+
+    This is a first-order proximal optimization algorithm for solving
+    a (possibly nonsmooth) convex optimization problem over the marginal polytope.
+    This is an  implementation of Algorithm 1 from the paper
+    ["Graphical-model based estimation and inference for differential privacy"]
+    (https://arxiv.org/pdf/1901.09136).  If stepsize is not provided, this algorithm
+    uses a line search to automatically choose appropriate step sizes that satisfy
+    the Armijo condition.
+
+    Args:
+        domain: The domain over which the model should be defined.
+        loss_fn: A MarginalLossFn or a list of Linear Measurements.
+        known_total: The known or estimated number of records in the data.
+        potentials: The initial potentials.  Must be defind over a set of cliques
+            that supports the cliques in the loss_fn.
+        marginal_oracle: The function to use to compute marginals from potentials.
+        iters: The maximum number of optimization iterations.
+        stepsize: The step size for the optimization.  If not provided, this algorithm
+            will use a line search to automatically choose appropriate step sizes.
+        callback_fn: A function to call at each iteration with the iteration number
+
+    Returns:
+        A GraphicalModel object with the estimated potentials and marginals.
+    """
+    loss_fn, known_total, potentials = _initialize(
+        domain, loss_fn, known_total, potentials
+    )
+
 
     @jax.jit
     def update(theta, alpha):
 
         mu = marginal_oracle(theta, known_total)
-        loss, dL = loss_and_grad(mu)
+        loss, dL = jax.value_and_grad(loss_fn)(mu)
 
         theta2 = theta - alpha * dL
+        if stepsize is not None:
+            return theta2, loss, alpha
+
         mu2 = marginal_oracle(theta2, known_total)
         loss2 = loss_fn(mu2)
 
@@ -99,10 +147,210 @@ def mirror_descent(
 
         return theta, loss, alpha
 
+    alpha = 2.0 if stepsize is None else stepsize 
     for t in range(iters):
-        potentials, loss, stepsize = update(potentials, stepsize)
-        # if t%50 == 0: print('Stepsize', stepsize)
+        potentials, loss, alpha = update(potentials, alpha)
         callback_fn(t, loss)
 
     marginals = marginal_oracle(potentials, known_total)
     return GraphicalModel(potentials, marginals, known_total)
+
+
+def lbfgs(
+    domain: Domain,
+    loss_fn: marginal_loss.MarginalLossFn | list[LinearMeasurement],
+    known_total: float | None = None,
+    potentials: CliqueVector | None = None,
+    marginal_oracle=marginal_oracles.message_passing_new,
+    iters: int = 1000,
+    callback_fn: Callable[[int, chex.Numeric], None] = _DEFAULT_CALLBACK,
+):
+    """Gradient-based optimization on the potentials (theta) via L-BFGS.
+
+    This optimizer works by calculating the gradients with respect to the
+    potentials by back-propagting through the marginal inference oracle.
+
+    This is a standard approach for fitting the parameters of a graphical model
+    without noise (i.e., when you know the exact marginals).  In this case,
+    the loss function with respect to theta is convex, and therefore this approach
+    enjoys convergence guarantees.  With generic marginal loss functions that arise
+    for instance iwth noise marginals, the loss function is typically convex with
+    respect to mu, but not with respect to theta.  Therefore, this optimizer is not
+    guaranteed to converge to the global optimum in all cases.  In practice, it
+    tends to work well in these settings despite non-convexities.  This approach
+    appeared in the paper ["Learning Graphical Model Parameters with Approximate
+    Marginal Inference"](https://arxiv.org/abs/1301.3193).
+
+    Args:
+      domain: The domain over which the model should be defined.
+      loss_fn: A MarginalLossFn or a list of Linear Measurements.
+      known_total: The known or estimated number of records in the data.
+        If loss_fn is provided as a list of LinearMeasurements, this argument
+        is optional.  Otherwise, it is required.
+      potentials: The initial potentials.  Must be defind over a set of cliques
+        that supports the cliques in the loss_fn.
+      marginal_oracle: The function to use to compute marginals from potentials.
+      iters: The maximum number of optimization iterations.
+      callback_fn
+    """
+    loss_fn, known_total, potentials = _initialize(
+        domain, loss_fn, known_total, potentials
+    )
+
+    # TODO: confirm that potentials.cliques supports loss_fn.cliques.
+
+    theta_loss = lambda theta: loss_fn(marginal_oracle(theta, known_total))
+    theta_loss_and_grad = jax.value_and_grad(theta_loss)
+
+    @jax.jit
+    def update(theta, opt_state):
+        loss, grad = theta_loss_and_grad(theta)
+        updates, opt_state = optimizer.update(
+            grad, opt_state, theta, value=loss, grad=grad, value_fn=theta_loss
+        )
+        return optax.apply_updates(theta, updates), opt_state, loss
+
+    optimizer = optax.lbfgs(
+        memory_size=1, linesearch=optax.scale_by_backtracking_linesearch(128)
+    )
+    state = optimizer.init(potentials)
+    for t in range(iters):
+        potentials, state, loss = update(potentials, state)
+        callback_fn(t, loss)
+
+    marginals = marginal_oracle(potentials, known_total)
+    return GraphicalModel(potentials, marginals, known_total)
+
+
+def mle_from_marginals(marginals: CliqueVector, known_total: float) -> GraphicalModel:
+    """Compute the MLE Graphical Model from the marginals.
+
+    Args:
+        marginals: The marginal probabilities.
+        known_total: The known or estimated number of records in the data.
+
+    Returns:
+        A GraphicalModel object with the final potentials and marginals.
+    """
+    negative_log_likelihood = lambda mu: -marginals.dot(mu.log())
+    loss_fn = marginal_loss.MarginalLossFn(marginals.cliques, negative_log_likelihood)
+    return lbfgs(marginals.domain, loss_fn, known_total)
+
+
+def dual_averaging(
+    domain: Domain,
+    loss_fn: marginal_loss.MarginalLossFn | list[LinearMeasurement],
+    lipschitz: float,
+    known_total: float | None = None,
+    potentials: CliqueVector | None = None,
+    marginal_oracle=marginal_oracles.message_passing_new,
+    iters: int = 1000,
+    callback_fn: Callable[[int, chex.Numeric], None] = _DEFAULT_CALLBACK,
+) -> GraphicalModel:
+    """Optimization using the Regularized Dual Averaging (RDA) algorithm.
+
+    RDA is an accelerated proximal algorithm for solving a smooth convex optimization
+    problem over the marginal polytope.  This algorithm requires knowledge of
+    the Lipschitz constant of the gradient of the loss function.
+
+    Args:
+        domain: The domain over which the model should be defined.
+        loss_fn: A MarginalLossFn or a list of Linear Measurements.
+        lipschitz: The Lipschitz constant of the gradient of the loss function.
+        known_total: The known or estimated number of records in the data.
+        potentials: The initial potentials.  Must be defind over a set of cliques
+            that supports the cliques in the loss_fn.
+        marginal_oracle: The function to use to compute marginals from potentials.
+        iters: The maximum number of optimization iterations.
+        callback_fn: A function to call at each iteration with the iteration number
+
+    Returns:
+        A GraphicalModel object with the final potentials and marginals.
+    """
+    loss_fn, known_total, potentials = _initialize(
+        domain, loss_fn, known_total, potentials
+    )
+
+    beta = 0
+    L = lipschitz
+
+    @jax.jit
+    def update(w, v, gbar, c):
+        u = (1 - c) * w + c * v
+        g = jax.grad(loss_fn)(u) * (1.0 / known_total)
+        gbar = (1 - c) * gbar + c * g
+        # not quite sure on the normalization by known_total here
+        theta = -t * (t + 1) / (4 * L + beta) * gbar
+        v = marginal_oracle(theta, known_total)
+        w = (1 - c) * w + c * v
+        return w, v, gbar
+
+    w = v = marginal_oracle(potentials, known_total)
+    gbar = CliqueVector.zeros(domain, loss_fn.cliques)
+    for t in range(1, iters + 1):
+        c = 2.0 / (t + 1)
+        w, v, gbar = update(w, v, gbar, c)
+
+    return mle_from_marginals(w, known_total)
+
+
+def interior_gradient(
+    domain: Domain,
+    loss_fn: marginal_loss.MarginalLossFn | list[LinearMeasurement],
+    lipschitz: float | None = None,
+    known_total: float | None = None,
+    potentials: CliqueVector | None = None,
+    marginal_oracle=marginal_oracles.message_passing_new,
+    iters: int = 1000,
+    stepsize: float | None = None,
+    callback_fn: Callable[[int, chex.Numeric], None] = _DEFAULT_CALLBACK,
+):
+    """Optimization using the Interior Point Gradient Descent algorithm.
+
+    Interior Gradient is an accelerated proximal algorithm for solving a smooth
+    convex optimization problem over the marginal polytope.  This algorithm
+    requires knowledge of the Lipschitz constant of the gradient of the loss function.
+    This algorithm is based on the paper titled
+    ["Interior Gradient and Proximal Methods for Convex and Conic Optimization"](https://epubs.siam.org/doi/abs/10.1137/S1052623403427823?journalCode=sjope8).
+
+    Args:
+        domain: The domain over which the model should be defined.
+        loss_fn: A MarginalLossFn or a list of Linear Measurements.
+        lipschitz: The Lipschitz constant of the gradient of the loss function.
+        known_total: The known or estimated number of records in the data.
+        potentials: The initial potentials.  Must be defind over a set of cliques
+            that supports the cliques in the loss_fn.
+        marginal_oracle: The function to use to compute marginals from potentials.
+        iters: The maximum number of optimization iterations.
+        callback_fn: A function to call at each iteration with the iteration number
+
+    Returns:
+        A GraphicalModel object with the optimized potentials and marginals.
+    """
+    loss_fn, known_total, potentials = _initialize(
+        domain, loss_fn, known_total, potentials
+    )
+
+    # Algorithm parameters
+    c = 1
+    sigma = 1
+    l = sigma / lipschitz
+
+    @jax.jit
+    def update(theta, c, x, y, z):
+        a = (((c * l) ** 2 + 4 * c * l) ** 0.5 - l * c) / 2
+        y = (1 - a) * x + a * z
+        c = c * (1 - a)
+        g = jax.grad(loss_fn)(y)
+        theta = theta - a / c / known_total * g
+        z = marginal_oracle(theta, known_total)
+        x = (1 - a) * x + a * z
+        return theta, c, x, y, z
+
+    x = y = z = marginal_oracle(potentials, known_total)
+    gbar = CliqueVector.zeros(domain, loss_fn.cliques)
+    theta = potentials
+    for t in range(1, iters + 1):
+        theta, c, x, y, z = update(theta, c, x, y, z)
+
+    return mle_from_marginals(x, known_total)
--- test/test_estimation.py
+++ test/test_estimation.py
@@ -5,6 +5,8 @@ import numpy as np
 from parameterized import parameterized
 import itertools
 
+np.random.seed(0)  # Avoid flaky tests
+
 _DOMAIN = Domain(["a", "b", "c", "d"], [2, 3, 4, 5])
 
 _CLIQUE_SETS = [
@@ -17,13 +19,13 @@ _CLIQUE_SETS = [
 ]
 
 
-def fake_measurements(cliques):
+def fake_measurements(cliques, norm="l2"):
     P = Factor.random(_DOMAIN)
     P = P / P.sum()
     measurements = []
     for cl in cliques:
         y = P.project(cl).datavector()
-        measurements.append(marginal_loss.LinearMeasurement(y, cl))
+        measurements.append(marginal_loss.LinearMeasurement(y, cl, loss=norm))
     return measurements
 
 
@@ -40,7 +42,20 @@ class TestEstimation(unittest.TestCase):
         measurements = fake_measurements(cliques)
         loss_fn = marginal_loss.from_linear_measurements(measurements)
 
-        model = estimation.mirror_descent(_DOMAIN, loss_fn, known_total=1.0, iters=500)
+        model = estimation.mirror_descent(_DOMAIN, loss_fn, known_total=1.0, iters=250)
+        for M in measurements:
+            expected = M.noisy_measurement
+            actual = model.project(M.clique).datavector()
+            np.testing.assert_allclose(actual, expected, atol=1e-2)
+
+    @parameterized.expand(itertools.product(_CLIQUE_SETS))
+    def test_mirror_descent_l1(self, cliques):
+        measurements = fake_measurements(cliques, norm="l1")
+        loss_fn = marginal_loss.from_linear_measurements(measurements)
+
+        model = estimation.mirror_descent(
+            _DOMAIN, loss_fn, known_total=1.0, iters=250, stepsize=0.001
+        )
         for M in measurements:
             expected = M.noisy_measurement
             actual = model.project(M.clique).datavector()
@@ -52,10 +67,60 @@ class TestEstimation(unittest.TestCase):
         potentials = CliqueVector.zeros(_DOMAIN, [_DOMAIN.attributes])
         loss_fn = marginal_loss.from_linear_measurements(measurements)
         joint_distribution = estimation.mirror_descent(
-            _DOMAIN, loss_fn, known_total=1.0, potentials=potentials
+            _DOMAIN, loss_fn, known_total=1.0, potentials=potentials, iters=250
         )
 
         for M in measurements:
             expected = M.noisy_measurement
             actual = joint_distribution.project(M.clique).datavector()
+            np.testing.assert_allclose(actual, expected, atol=1e-2)
+
+    @parameterized.expand(itertools.product(_CLIQUE_SETS))
+    def test_primal_optimization(self, cliques):
+        measurements = fake_measurements(cliques)
+        loss_fn = marginal_loss.from_linear_measurements(measurements)
+
+        model = estimation.lbfgs(_DOMAIN, loss_fn, known_total=1.0, iters=250)
+        for M in measurements:
+            expected = M.noisy_measurement
+            actual = model.project(M.clique).datavector()
+            np.testing.assert_allclose(actual, expected, atol=1e-2)
+
+    @parameterized.expand(itertools.product(_CLIQUE_SETS))
+    def test_dual_averaging(self, cliques):
+        measurements = fake_measurements(cliques)
+        loss_fn = marginal_loss.from_linear_measurements(measurements)
+
+        L = 0.001  # Currently treating Lipschitz constant as a hyperparameter
+        model = estimation.dual_averaging(
+            _DOMAIN, loss_fn, lipschitz=L, known_total=1.0, iters=250
+        )
+        for M in measurements:
+            expected = M.noisy_measurement
+            actual = model.project(M.clique).datavector()
+            np.testing.assert_allclose(actual, expected, atol=1e-2)
+
+    @parameterized.expand(itertools.product(_CLIQUE_SETS))
+    def test_interior_gradient(self, cliques):
+        measurements = fake_measurements(cliques)
+        loss_fn = marginal_loss.from_linear_measurements(measurements)
+
+        L = 0.01  # Currently treating Lipschitz constant as a hyperparameter
+        model = estimation.interior_gradient(
+            _DOMAIN, loss_fn, lipschitz=L, known_total=1.0, iters=250
+        )
+        for M in measurements:
+            expected = M.noisy_measurement
+            actual = model.project(M.clique).datavector()
+
+    @parameterized.expand(itertools.product(_CLIQUE_SETS))
+    def test_mle(self, cliques):
+        P = Factor.random(_DOMAIN) * 10
+        total = float(P.sum())
+        mu = CliqueVector(_DOMAIN, cliques, {cl: P.project(cl) for cl in cliques})
+
+        model = estimation.mle_from_marginals(mu, known_total=total)
+        for cl in cliques:
+            expected = mu.project(cl).datavector()
+            actual = model.project(cl).datavector()
             np.testing.assert_allclose(actual, expected, atol=1e-2)
