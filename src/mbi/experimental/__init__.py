8f4579faf018e0f1b66b7a04f4cdb77cee2db8de
Ryan McKenna <rmckenna21@gmail.com>
2024-11-15 19:51:51 -0800

    cleanup experimental

--- src/mbi/experimental/__init__.py
+++ src/mbi/experimental/__init__.py
@@ -1,12 +1,2 @@
-from .factor_graph import FactorGraph
-from .region_graph import RegionGraph
-from .local_inference import LocalInference
 from .public_inference import PublicInference
-
-try:
-    from .mixture_inference import MixtureInference
-except:
-    import warnings
-
-    warnings.warn("MixtureInference disabled, please install jax and jaxlib")
-
+from .mixture_inference import MixtureInference
